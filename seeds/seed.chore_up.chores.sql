9a87a0f50de27f0076f8a98d9b102bd5d9d96fec
Ryan <leeyuenhigh@gmail.com>
2021-03-26 03:45:33 -0400

    Created seed file for chores table

---
+++ seeds/seed.chore_up.chores.sql
@@ -0,0 +1,10 @@
+INSERT INTO chores(id,name,value,status,comments)
+VALUES
+  (1,'Walk the dog',10,'Incomplete',''),
+  (2,'Take Jack to school',20,'Incomplete',''),
+  (3,'Go to the PO box',20,'Incomplete',''),
+  (4,'Buy groceries',20,'Incomplete',''),
+  (5,'Water the plants',10,'Incomplete',''),
+  (6,'Do the laundry',30,'Incomplete',''),
+  (7,'Take out the trash',10,'Incomplete','')
+;
