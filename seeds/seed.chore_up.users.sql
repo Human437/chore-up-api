10508e0ae4e806ee8b78d0ce8fdd0403c87bebf2
Ryan <leeyuenhigh@gmail.com>
2021-03-26 03:45:46 -0400

    Created seed file for userstable

---
+++ seeds/seed.chore_up.users.sql
@@ -0,0 +1,6 @@
+INSERT INTO users(id,name,chores,level,xp_till_level_up,email,hashedpassword)
+VALUES
+  (1,'Bob','{1,2,3}',1,100,'hwpyxoutfugfqbusvz@twzhhq.com','$2a$10$l1mqfAFPUolxGgJKL5tXHu4PUSoBNoD0JlM45RsBcaIcWkPa2UD0C'),
+  (2,'Jack','{4,5}',1,100,'hwpyxoutfugfqbusvz@twzhhq.com','$2a$10$l1mqfAFPUolxGgJKL5tXHu4PUSoBNoD0JlM45RsBcaIcWkPa2UD0C'),
+  (3,'Jill','{6,7}',1,100,'hwpyxoutfugfqbusvz@twzhhq.com','$2a$10$l1mqfAFPUolxGgJKL5tXHu4PUSoBNoD0JlM45RsBcaIcWkPa2UD0C')
+;
