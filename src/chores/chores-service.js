2d8390744a9b0b2db92b52453e392062b6245902
Ryan <leeyuenhigh@gmail.com>
2021-03-28 16:24:56 -0400

    Created service object for chores

---
+++ src/chores/chores-service.js
@@ -0,0 +1,24 @@
+const ChoresService = {
+  getChoreById(knex,id){
+    return knex.from('chores').select('*').where('id',id).first()
+  },
+  insertChore(knex,chore){
+    return knex
+      .insert(chore)
+      .into('chores')
+      .returning('*')
+      .then(rows => {
+        return rows[0]
+      })
+  },
+  updateChore(knex,id,newChoreFields){
+    return knex('chores')
+      .where({id})
+      .update(newChoreFields)
+  },
+  deleteChore(knex,id){
+    return knex('chores')
+      .where({id})
+      .delete()
+  }
+}
