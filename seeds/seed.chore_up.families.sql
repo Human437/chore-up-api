8643aff7f1923ce700c3fe801c2d1428bc42b833
Ryan <leeyuenhigh@gmail.com>
2021-03-28 15:58:29 -0400

    Fixed field names

--- seeds/seed.chore_up.families.sql
+++ seeds/seed.chore_up.families.sql
@@ -1,4 +1,4 @@
-INSERT INTO families(id,admin,code_2_join)
+INSERT INTO families(id,admin,code_to_join)
 VALUES
   (1,1,'aJk3T')
 ;
--- seeds/seed.chore_up.users.sql
+++ seeds/seed.chore_up.users.sql
@@ -1,4 +1,4 @@
-INSERT INTO users(id,name,level,xp_till_level_up,email,hashedpassword)
+INSERT INTO users(id,name,level,xp_till_level_up,email,hashed_password)
 VALUES
   (1,'Bob',1,100,'hwpyxoutfugfqbusvz@twzhhq.com','$2a$10$l1mqfAFPUolxGgJKL5tXHu4PUSoBNoD0JlM45RsBcaIcWkPa2UD0C'),
   (2,'Jack',1,100,'hwpyxoutfugfqbusvz@twzhhq.com','$2a$10$l1mqfAFPUolxGgJKL5tXHu4PUSoBNoD0JlM45RsBcaIcWkPa2UD0C'),
