152b165f4c0f8567a258734512c5f4dc387c317b
Ryan <leeyuenhigh@gmail.com>
2021-03-28 21:16:33 -0400

    Created service object for users

---
+++ src/users/users-service.js
@@ -0,0 +1,26 @@
+const UsersService = {
+  getUserById(knex,id){
+    return knex.from('users').select('*').where('id',id).first()
+  },
+  insertUser(knex,user){
+    return knex
+      .insert(user)
+      .into('users')
+      .returning('*')
+      .then(rows => {
+        return rows[0]
+      })
+  },
+  updateUser(knex,id,newUserFields){
+    return knex('users')
+      .where({id})
+      .update(newUserFields)
+  },
+  deleteUser(knex,id){
+    return knex('users')
+      .where({id})
+      .delete()
+  }
+}
+
+module.exports = UsersService
