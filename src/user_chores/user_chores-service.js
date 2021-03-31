0168f2b7eec242cb18eacda243ef0ca74e82dfdf
Ryan <leeyuenhigh@gmail.com>
2021-03-31 01:15:51 -0400

    Updated service object for user_chores

--- src/user_chores/user_chores-service.js
+++ src/user_chores/user_chores-service.js
@@ -1,6 +1,25 @@
 const User_ChoresService = {
   getAllChoresByUserId(knex,userId){
     return knex.from('chores').leftJoin('user_chores','chores.id','user_chores.chore_id').select('*').where('user_chores.user_id',userId)
+  },
+  insertUser_Chore(knex,User_Chore){
+    return knex
+    .insert(User_Chore)
+    .into('user_chores')
+    .returning('*')
+    .then(rows => {
+      return rows[0]
+    })  
+  },
+  updateUser_Chore(knex,id,newUser_ChoreFields){
+    return knex('user_chores')
+      .where({id})
+      .update(newUser_ChoreFields)
+  },
+  deleteUser_Chore(knex,id){
+    return knex('user_chores')
+      .where({id})
+      .delete()
   }
 }
 
