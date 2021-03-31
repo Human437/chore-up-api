73d6910f41c47a5afe3457d2169f8d1ff56a2e14
Ryan <leeyuenhigh@gmail.com>
2021-03-31 04:04:37 -0400

    Finished service object for family_members

--- src/family_members/family_members-service.js
+++ src/family_members/family_members-service.js
@@ -5,6 +5,28 @@ const Family_MembersService = {
       .leftJoin('family_members','users.id','family_members.family_id')
       .select('*')
       .where('family_members.family_id',familyId)
+  },
+  insertFamily_Member(knex,Family_Member){
+    return knex
+    .insert(Family_Member)
+    .into('family_members')
+    .returning('*')
+    .then(rows => {
+      return rows[0]
+    })  
+  },
+  updateFamily_Member(knex,id,newFamily_MemberFields){
+    return knex('family_members')
+      .where({id})
+      .update(newFamily_MemberFields)
+  },
+  deleteFamily_Member(knex,id){
+    return knex('family_members')
+      .where({id})
+      .delete()
+  },
+  getFamily_MembersById(knex,id){
+    return knex.from('family_members').select('*').where('id',id).first()
   }
 }
 
