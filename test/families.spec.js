830126f6e77f38b685b0d5c17536f054397d623f
Ryan <leeyuenhigh@gmail.com>
2021-04-02 20:34:56 -0400

    Wrote happy test cases for PATCH /api/families/:familyId

--- test/families.spec.js
+++ test/families.spec.js
@@ -17,9 +17,11 @@ describe('Families Endpoints', () => {
 
   after('disconnect from db', () => db.destroy())
 
+  before('cleanup', () => db('users').delete())
   before('cleanup', () => db('families').delete())
 
   afterEach('cleanup', () => db('families').delete())
+  afterEach('cleanup', () => db('users').delete())
 
   describe('GET /api/families/:familyId', () => {
     context('Given there are families in the database', () => {
@@ -45,6 +47,69 @@ describe('Families Endpoints', () => {
           .get(`/api/families/${familyId}`)
           .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
           .expect(200, expectedFamily)
+      })
+    })
+  })
+
+  describe('PATCH /api/families/:familyId', () => {
+    context('Given there are families in the database', () => {
+      const testFamilies = fixtures.makeFamiliesArray()
+      const testUsers = fixtures.makeUsersArray()
+      const familyId = 1
+
+      beforeEach('insert users', () => {
+        return db
+          .into('users')
+          .insert(testUsers)
+      })
+
+      beforeEach('insert families', () => {
+        return db
+          .into('families')
+          .insert(testFamilies)
+      })
+
+      it('responds with 204 and updates the db when all fields are provided', () =>{
+        const updatedFamily = {
+          admin: 2,
+          code_to_join:"test"
+        }
+        const expectedFamily = {
+          ...testFamilies[familyId -1],
+          ...updatedFamily
+        }
+        return supertest(app)
+          .patch(`/api/families/${familyId}`)
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .send(updatedFamily)
+          .expect(204)
+          .then(res =>
+            supertest(app)
+              .get(`/api/families/${familyId}`)
+              .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+              .expect(expectedFamily)
+          )
+      })
+
+      it('responds with 204 and updates the db when some fields are provided', () =>{
+        const updatedFamily = {
+          code_to_join:"test"
+        }
+        const expectedFamily = {
+          ...testFamilies[familyId -1],
+          ...updatedFamily
+        }
+        return supertest(app)
+          .patch(`/api/families/${familyId}`)
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .send(updatedFamily)
+          .expect(204)
+          .then(res =>
+            supertest(app)
+              .get(`/api/families/${familyId}`)
+              .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+              .expect(expectedFamily)
+          )
       })
     })
   })
