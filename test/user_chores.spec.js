bbf55deaaf9454e5006af78bee63287a1c748303
Ryan <leeyuenhigh@gmail.com>
2021-04-03 22:46:54 -0400

    Wrote happy test case for PATCH /api/user_chores/:user_choreId

--- test/user_chores.spec.js
+++ test/user_chores.spec.js
@@ -17,9 +17,11 @@ describe('User_Chores Endpoints', () => {
 
   after('disconnect from db', () => db.destroy())
 
+  before('cleanup', () => db('user_chores').delete())
   before('cleanup', () => db('users').delete())
   before('cleanup', () => db('chores').delete())
 
+  afterEach('cleanup', () => db('user_chores').delete())
   afterEach('cleanup', () => db('users').delete())
   afterEach('cleanup', () => db('chores').delete())
 
@@ -54,6 +56,75 @@ describe('User_Chores Endpoints', () => {
           .get(`/api/user_chores/${user_choreId}`)
           .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
           .expect(200, expectedUser_Chores)
+      })
+    })
+  })
+  describe('PATCH /api/user_chores/:user_choreId', () => {
+    context('Given there are user_chores in the database', () => {
+      const testUser_Chores = fixtures.makeUser_ChoresArray()
+      const testUsers = fixtures.makeUsersArray()
+      const testChores = fixtures.makeChoresArray()
+      const user_choreId = 1
+
+      beforeEach('insert users', () => {
+        return db
+          .into('users')
+          .insert(testUsers)
+      })
+
+      beforeEach('insert chores', () => {
+        return db
+          .into('chores')
+          .insert(testChores)
+      })
+
+      beforeEach('insert user_chores', () => {
+        return db
+          .into('user_chores')
+          .insert(testUser_Chores)
+      })
+
+      it('responds with 204 and updates the db when all fields are provided', () =>{
+        const updatedUser_Chore = {
+          user_id: 3,
+          chore_id: 1
+        }
+        const expectedUser_Chore = {
+          ...testUser_Chores[user_choreId -1],
+          ...updatedUser_Chore
+        }
+        return supertest(app)
+          .patch(`/api/user_chores/${user_choreId}`)
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .send(updatedUser_Chore)
+          .expect(204)
+          .then(res =>
+            supertest(app)
+              .get(`/api/user_chores/${user_choreId}`)
+              .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+              .expect(expectedUser_Chore)
+          )
+      })
+
+      it('responds with 204 and updates the db when some fields are provided', () =>{
+        const updatedUser_Chore = {
+          user_id: 3
+        }
+        const expectedUser_Chore = {
+          ...testUser_Chores[user_choreId -1],
+          ...updatedUser_Chore
+        }
+        return supertest(app)
+          .patch(`/api/user_chores/${user_choreId}`)
+          .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+          .send(updatedUser_Chore)
+          .expect(204)
+          .then(res =>
+            supertest(app)
+              .get(`/api/user_chores/${user_choreId}`)
+              .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+              .expect(expectedUser_Chore)
+          )
       })
     })
   })
