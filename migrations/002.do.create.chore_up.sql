1f1b543563853f1ae3290da769d5e01ed7af5854
Ryan <leeyuenhigh@gmail.com>
2021-04-05 06:27:49 -0400

    Added new is_admin field for users table

--- migrations/002.do.create.chore_up.sql
+++ migrations/002.do.create.chore_up.sql
@@ -5,6 +5,7 @@ CREATE TABLE users(
   name TEXT NOT NULL,
   level NUMERIC NOT NULL,
   xp_till_level_up NUMERIC NOT NULL,
+  is_admin BOOLEAN NOT NULL,
   email TEXT NOT NULL,
   hashed_password TEXT NOT NULL
 );
