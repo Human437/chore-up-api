9457e07d58e07217a27e01a6d564742e9701daf4
Ryan <leeyuenhigh@gmail.com>
2021-04-05 08:12:38 -0400

    Updated seed file with is_admin fields

--- seeds/seed.chore_up.users.sql
+++ seeds/seed.chore_up.users.sql
@@ -1,6 +1,6 @@
-INSERT INTO users(id,name,level,xp_till_level_up,email,hashed_password)
+INSERT INTO users(id,name,level,xp_till_level_up,is_admin,email,hashed_password)
 VALUES
-  (1,'Bob',1,100,'hwpyxoutfugfqbusvz@twzhhq.com','$2a$10$l1mqfAFPUolxGgJKL5tXHu4PUSoBNoD0JlM45RsBcaIcWkPa2UD0C'),
-  (2,'Jack',1,100,'hwpyxoutfugfqbusvz@twzhhq.com','$2a$10$l1mqfAFPUolxGgJKL5tXHu4PUSoBNoD0JlM45RsBcaIcWkPa2UD0C'),
-  (3,'Jill',1,100,'hwpyxoutfugfqbusvz@twzhhq.com','$2a$10$l1mqfAFPUolxGgJKL5tXHu4PUSoBNoD0JlM45RsBcaIcWkPa2UD0C')
+  (1,'Bob',1,100,true,'hwpyxoutfugfqbusvz@twzhhq.com','$2a$10$l1mqfAFPUolxGgJKL5tXHu4PUSoBNoD0JlM45RsBcaIcWkPa2UD0C'),
+  (2,'Jack',1,100,false,'hwpyxoutfugfqbusvz@twzhhq.com','$2a$10$l1mqfAFPUolxGgJKL5tXHu4PUSoBNoD0JlM45RsBcaIcWkPa2UD0C'),
+  (3,'Jill',1,100,false,'hwpyxoutfugfqbusvz@twzhhq.com','$2a$10$l1mqfAFPUolxGgJKL5tXHu4PUSoBNoD0JlM45RsBcaIcWkPa2UD0C')
 ;
