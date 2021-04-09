3b80c0a7d1007802af01107063abcb3923b9a935
Ryan <leeyuenhigh@gmail.com>
2021-04-09 03:26:35 -0400

    Updated seed file to use done

--- seeds/seed.chore_up.chores.sql
+++ seeds/seed.chore_up.chores.sql
@@ -1,10 +1,10 @@
-INSERT INTO chores(id,name,value,status,comments)
+INSERT INTO chores(id,name,value,done,comments)
 VALUES
-  (1,'Walk the dog',10,'Incomplete',''),
-  (2,'Take Jack to school',20,'Incomplete',''),
-  (3,'Go to the PO box',20,'Incomplete',''),
-  (4,'Buy groceries',20,'Incomplete',''),
-  (5,'Water the plants',10,'Incomplete',''),
-  (6,'Do the laundry',30,'Incomplete',''),
-  (7,'Take out the trash',10,'Incomplete','')
+  (1,'Walk the dog',10,false,''),
+  (2,'Take Jack to school',20,false,''),
+  (3,'Go to the PO box',20,false,''),
+  (4,'Buy groceries',20,false,''),
+  (5,'Water the plants',10,false,''),
+  (6,'Do the laundry',30,false,''),
+  (7,'Take out the trash',10,false,'')
 ;
