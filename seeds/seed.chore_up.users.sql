0eb25731ed87e18094b734a9818de41397e5b928
Ryan <leeyuenhigh@gmail.com>
2021-04-16 03:06:40 -0400

    Fixed seed file so that the emails are not exactly the same

--- seeds/seed.chore_up.users.sql
+++ seeds/seed.chore_up.users.sql
@@ -1,6 +1,6 @@
 INSERT INTO users(name,level,xp_till_level_up,is_admin,email,hashed_password)
 VALUES
   ('Bob',1,100,true,'hwpyxoutfugfqbusvz@twzhhq.com','$2a$10$l1mqfAFPUolxGgJKL5tXHu4PUSoBNoD0JlM45RsBcaIcWkPa2UD0C'),
-  ('Jack',1,100,false,'hwpyxoutfugfqbusvz@twzhhq.com','$2a$10$l1mqfAFPUolxGgJKL5tXHu4PUSoBNoD0JlM45RsBcaIcWkPa2UD0C'),
-  ('Jill',1,100,false,'hwpyxoutfugfqbusvz@twzhhq.com','$2a$10$l1mqfAFPUolxGgJKL5tXHu4PUSoBNoD0JlM45RsBcaIcWkPa2UD0C')
+  ('Jack',1,100,false,'hwpyxoutfugfqbusvz@twzhh.com','$2a$10$l1mqfAFPUolxGgJKL5tXHu4PUSoBNoD0JlM45RsBcaIcWkPa2UD0C'),
+  ('Jill',1,100,false,'hwpyxoutfugfqbusvz@twzh.com','$2a$10$l1mqfAFPUolxGgJKL5tXHu4PUSoBNoD0JlM45RsBcaIcWkPa2UD0C')
 ;
